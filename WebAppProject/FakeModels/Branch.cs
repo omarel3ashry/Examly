fa375bd9a61541377591b03d46fad4394d622da4
Mohamed 3bdelrahman <mohamedbdelrahman2@gmail.com>
2024-03-11 15:29:19 +0200

    Create admin manage page

---
+++ WebAppProject/FakeModels/Branch.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAppProject.FakeModels
+{
+    public class Branch
+    {   
+        public int BranchId { get; set; }
+        public string Name { get; set; }     
+    }
+}
---
+++ WebAppProject/FakeModels/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppProject.FakeModels
+{
+    public class Department
+    {   
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public int BranchId { get; set; }
+        public Branch Branch { get; set; }
+    }
+}
