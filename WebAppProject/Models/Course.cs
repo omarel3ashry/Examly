c902c2f86edf4a96d3769548b80d7e1af2ad86ed
hossameltayeb83 <96459585+hossameltayeb83@users.noreply.github.com>
2024-03-12 13:33:05 +0200

    Delete WebAppProject/Models/Course.cs

--- WebAppProject/Models/Course.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAppProject.Models
-{
-    public class Course
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-    }
-}
